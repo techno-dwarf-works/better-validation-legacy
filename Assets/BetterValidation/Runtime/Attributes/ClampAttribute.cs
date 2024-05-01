d83c3787d8c02ebd03d86957af5e21ce406d1f8b
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-05-01 20:51:02 +0200

    Add MaxAttribute and ClampAttribute

---
+++ Assets/BetterValidation/Runtime/Attributes/ClampAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using Better.Internal.Core.Runtime;
+
+namespace Better.Validation.Runtime.Attributes
+{
+    [Conditional(Defines.Editor)]
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ClampAttribute : ValidationAttribute
+    {
+        public float Min { get; }
+        public float Max { get; }
+
+        public ClampAttribute(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
