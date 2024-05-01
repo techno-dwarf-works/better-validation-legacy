3d8a3e4c6f330aaa5427fbdbe45476a426ef1fc1
OpOpYaDev <OpOpYaDev@users.noreply.github.com>
2024-05-01 18:52:58 +0000

    Merge pull request #28 from techno-dwarf-works/dev
    Version 1.6.1

---
+++ Editor/EditorAddons/Wrappers/MaxWrapper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Better.Commons.EditorAddons.Drawers.Caching;
+using Better.Commons.EditorAddons.Extensions;
+using Better.Commons.EditorAddons.Utility;
+using Better.Validation.Runtime.Attributes;
+using UnityEditor;
+
+namespace Better.Validation.EditorAddons.Wrappers
+{
+    public class MaxWrapper : PropertyValidationWrapper
+    {
+        public override bool IsSupported()
+        {
+            return Property.propertyType is SerializedPropertyType.Integer or SerializedPropertyType.Float;
+        }
+
+        public override CacheValue<string> Validate()
+        {
+            var maxAttribute = (MaxAttribute)Attribute;
+            var maxValue = maxAttribute.Max;
+            var isValid = Property.propertyType switch
+            {
+                SerializedPropertyType.Float => Property.floatValue <= maxValue,
+                SerializedPropertyType.Integer => Property.intValue <= maxValue,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            if (isValid)
+            {
+                return GetClearCache();
+            }
+
+            switch (Property.propertyType)
+            {
+                case SerializedPropertyType.Float:
+                    Property.SetValue(maxValue);
+                    break;
+                case SerializedPropertyType.Integer:
+                    Property.SetValue((int)maxValue);
+                    break;
+            }
+
+            EditorUtility.SetDirty(Property.serializedObject.targetObject);
+            Property.serializedObject.ApplyModifiedProperties();
+            return GetClearCache();
+        }
+    }
+}
---
+++ Runtime/Attributes/MaxAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using Better.Internal.Core.Runtime;
+
+namespace Better.Validation.Runtime.Attributes
+{
+    [Conditional(Defines.Editor)]
+    [AttributeUsage(AttributeTargets.Field)]
+    public class MaxAttribute : ValidationAttribute
+    {
+        public float Max { get; }
+
+        public MaxAttribute(float max)
+        {
+            Max = max;
+        }
+    }
+}
